8df91f407138f0fb5c1c74c604c3070733590c2f
Luiz Suzana <luiz.suzana@mipwise.com>
2022-05-11 10:37:46 -0300

    Added protection against bad 'Period ID' columns
    In utils.py, created check_each_period_id_column() function to ensure that each of these columns have integer values from 1 to the biggest, without any gap, but not necessarily ordered.
    
    Included this verification in main.solve()

---
+++ pastesian/utils.py
@@ -0,0 +1,39 @@
+from pastesian import input_schema
+
+
+def check_each_period_id_column(dat):
+    """
+    Check if all tables in dat object have the appropriate 'Period ID' column, i.e., integer numbers from 1 to the
+    number of rows in the corresponding table, without any gap (but not necessarily ordered).
+
+    Examples: I = [1, 4, 7, 5, 6, 3, 2] is a valid 'Period ID' column, while I = [3, 5, 4, 1] is not because number 2
+    is missing in the latter.
+
+    :param dat: PanDat object containing the input data, accordingly to the input_schema.
+
+    :return:
+    """
+    field = 'Period ID'
+    for table_name in input_schema.all_tables:
+        table = dat.__getattribute__(table_name)
+
+        # if hasattr(table, field):
+        #     pass
+
+        if field in table.columns:
+            I = list(table[field])
+            I.sort()
+            if not I[0] == 1 or not I[-1] == len(I):
+                error_msg = f"'{field}' column at '{table_name}' table must have all integers from 1 to the biggest " \
+                            f"period index, not necessarily ordered though."
+                raise ValueError(error_msg)
+
+
+def check_period_id_matching():
+    """
+    Check if all tables share the same 'Period ID' column, except possibly
+
+    :return:
+    """
+
+    pass
