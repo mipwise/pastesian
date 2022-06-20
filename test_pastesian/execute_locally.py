cfb7e2f5c7dee9546366e2033b9615c8929488df
Luiz Suzana <luiz.suzana@mipwise.com>
2022-06-20 16:30:13 -0300

    Fixed ImportError and bad paths on execute_locally.py

--- test_pastesian/execute_locally.py
+++ test_pastesian/execute_locally.py
@@ -1,9 +1,10 @@
 from pastesian import input_schema, output_schema
 from pastesian import solve
 from test_pastesian.action_local_data_integrity import local_data_integrity_check, local_fix_bad_data
+import os
 
 # Creating "dat" object, containing all input data
-input_path = "data/inputs"
+input_path = os.path.join(os.path.dirname(__file__), "data/inputs")
 dat = input_schema.csv.create_pan_dat(input_path)
 
 # Checking data integrity
@@ -13,5 +14,5 @@ local_data_integrity_check(dat)
 sln = solve(dat)
 
 # Populate the output schema's tables
-output_path = "data/outputs"
+output_path = os.path.join(os.path.dirname(__file__), "data/outputs")
 output_schema.csv.write_directory(sln, output_path)
---
+++ test_pastesian/testing_pastesian.py
@@ -0,0 +1,11 @@
+import pastesian
+import unittest
+
+
+class TestPastesian(unittest.TestCase):
+    def test_main_solve(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
