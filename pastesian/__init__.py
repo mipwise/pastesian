a3732ff2ea09adf5fdc074f2281c2c9a78780c29
Luiz Suzana <luiz.suzana@mipwise.com>
2022-06-28 09:46:18 -0300

    Updated __version__ to 0.1.4

--- pastesian/__init__.py
+++ pastesian/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 from pastesian.schemas import input_schema, output_schema
 from pastesian.action_update_demand import action_update_demand
 from pastesian.main import solve
