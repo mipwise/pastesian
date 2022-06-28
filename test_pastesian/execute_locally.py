1d05b29ab2263ca3d61f245c84316ca7942a9746
Luiz Suzana <luiz.suzana@mipwise.com>
2022-06-28 09:11:40 -0300

    Added _this_directory() function to execute_locally.py

--- test_pastesian/execute_locally.py
+++ test_pastesian/execute_locally.py
@@ -2,9 +2,15 @@ from pastesian import input_schema, output_schema
 from pastesian import solve
 from test_pastesian.action_local_data_integrity import local_data_integrity_check
 import os
+import inspect
+
+
+def _this_directory():
+    return os.path.dirname(os.path.realpath(os.path.abspath(inspect.getsourcefile(_this_directory))))
+
 
 # Create "dat" object, containing all input data
-input_path = os.path.join(os.path.dirname(__file__), "data/inputs")
+input_path = os.path.join(_this_directory(), "data/inputs")
 dat = input_schema.csv.create_pan_dat(input_path)
 
 # Check data integrity
@@ -14,5 +20,5 @@ local_data_integrity_check(dat)
 sln = solve(dat)
 
 # Populate the output schema's tables
-output_path = os.path.join(os.path.dirname(__file__), "data/outputs")
+output_path = os.path.join(_this_directory(), "data/outputs")
 output_schema.csv.write_directory(sln, output_path)
