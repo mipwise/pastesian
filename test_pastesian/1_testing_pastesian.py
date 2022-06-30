f24361ee2c16d9062046958be9171eeb425a5628
Luiz Suzana <luiz.suzana@mipwise.com>
2022-06-29 21:35:55 -0300

    Fixed previous tests, now read data from json separated files.

--- test_pastesian/1_testing_pastesian.py
+++ test_pastesian/1_testing_pastesian.py
@@ -1,14 +1,15 @@
 """
-Module to run unit tests for pastesian package.
+Module to run unit tests for pastesian package, using data from '1' prefix inside test_pastesian/data/testing_data,
+i.e., the input data to these tests is 1_input.json and the output is 1_output.json
 
-Commands for the command-line:
-
-- coverage run <test_file.py>
+To run this in the command line, use "python -m unittest 1_testing_pastesian.py", or alternatively to get reports on
+how much of the code from pastesian package has been tested, use the 'coverage' package through these three commands:
+- coverage run 1_testing_pastesian.py
 - coverage report
 - coverage html
 """
 
-from pastesian import action_update_demand, solve, input_schema
+from pastesian import action_update_demand, solve, input_schema, output_schema
 from pastesian.utils import check_each_period_id_column
 from pastesian.main import create_optimization_parameters
 import unittest
@@ -24,11 +25,17 @@ def _this_directory():
 
 class TestPastesian(unittest.TestCase):
     period_id_field_name = 'Period ID'
-    input_path = os.path.join(_this_directory(), "data/inputs")
+    input_data_path = os.path.join(_this_directory(), "data/testing_data/1_input.json")
+    output_data_path = os.path.join(_this_directory(), "data/testing_data/1_output.json")
 
     @classmethod
-    def setUp(cls) -> None:
-        cls.dat = input_schema.csv.create_pan_dat(cls.input_path)
+    def setUpClass(cls) -> None:
+        cls.dat = input_schema.json.create_pan_dat(cls.input_data_path)
+        cls.sln = output_schema.json.create_pan_dat(cls.output_data_path)
+
+    def test_local_data_integrity_check(self):
+        # TODO: print data integrity checks, and then make asserts as mip_me does
+        pass
 
     def test_create_optimization_parameters(self):
         # Original data set
@@ -39,21 +46,21 @@ class TestPastesian(unittest.TestCase):
         pc_expected = {1: 5.5, 2: 7.2, 3: 8.8, 4: 10.9}
         ic_expected = {1: 1.3, 2: 1.95, 3: 2.2, 4: 2.0}
         I_expected = [1, 2, 3, 4]
+
         self.assertTrue(all(isclose(d[key], d_expected[key], rel_tol=1e-2) for key in d_expected))
         self.assertTrue(all(isclose(pc[key], pc_expected[key], rel_tol=1e-2) for key in pc_expected))
         self.assertTrue(all(isclose(ic[key], ic_expected[key], rel_tol=1e-2) for key in ic_expected))
         self.assertListEqual(I, I_expected)
 
-        # demand['Period ID'] with more entries than costs['Period ID']
+        # Sample 2: demand['Period ID'] with more entries than costs['Period ID'], expected ValueError
         dat2 = input_schema.copy_pan_dat(self.dat)  # copy PanDat object
         demand_copy = dat2.demand.copy()
         demand_copy = pd.concat([demand_copy, pd.DataFrame([[5, 200]], columns=['Period ID', 'Demand'])])  # add new row
         dat2.demand = demand_copy  # update 'demand' table in dat2 PanDat object
-
         with self.assertRaises(ValueError):
             d, pc, ic, I = create_optimization_parameters(dat2)
 
-        # costs['Period ID'] with more entries than demand['Period ID']
+        # Sample 3: costs['Period ID'] with more entries than demand['Period ID'], expected ValueError
         dat3 = input_schema.copy_pan_dat(self.dat)  # copy PanDat object
         costs_copy = dat3.costs.copy()
         costs_copy = pd.concat([costs_copy, pd.DataFrame(
@@ -61,7 +68,6 @@ class TestPastesian(unittest.TestCase):
             )
         ])  # add new row
         dat3.costs = costs_copy  # update 'costs' table in dat3 PanDat object
-
         with self.assertRaises(ValueError):
             d, pc, ic, I = create_optimization_parameters(dat3)
 
@@ -70,8 +76,8 @@ class TestPastesian(unittest.TestCase):
         sln = solve(self.dat)
 
         # Expected output data
-        production_flow_expected = pd.read_csv(os.path.join(_this_directory(), "data/outputs/production_flow.csv"))
-        costs_expected = pd.read_csv(os.path.join(_this_directory(), "data/outputs/costs.csv"))
+        production_flow_expected = self.sln.production_flow
+        costs_expected = self.sln.costs
 
         # Testing
         self.assertIsNone(pd.testing.assert_frame_equal(sln.production_flow, production_flow_expected,
@@ -81,39 +87,33 @@ class TestPastesian(unittest.TestCase):
 
     def test_action_update_demand(self):
         # Safe operation
-        demand_copy = self.dat.demand.copy()
-        multiplier = -1  # default value for "Production Capacity", which is used in action_update_demand
-        demand_copy['Demand'] = demand_copy['Demand'] * multiplier
+        demand_expected = self.dat.demand.copy()
+        multiplier = -1  # default value for "Production Capacity", used in action_update_demand
+        demand_expected['Demand'] = demand_expected['Demand'] * multiplier
 
         # Operation to test
-        self.dat = action_update_demand(self.dat)
+        demand_to_test = action_update_demand(self.dat).demand
 
-        num_rows = demand_copy.shape[0]
-        for index in range(num_rows):
-            action_output = self.dat.demand.at[index, 'Demand']
-            expected_output = demand_copy.at[index, 'Demand']
-            self.assertTrue(isclose(action_output, expected_output, rel_tol=1e-2),
-                            msg=f'action: {action_output}, expected: {expected_output}')
+        # Testing
+        self.assertIsNone(pd.testing.assert_frame_equal(demand_expected, demand_to_test, check_dtype=False,
+                                                        rtol=1.0e-5, atol=1.0e-8))
 
     def test_check_each_period_id_column(self):
         # Original good demand.csv
         self.assertIsNone(check_each_period_id_column(self.dat))
 
-        # Non-integers on demand['Period ID']
-        self.dat.demand[self.period_id_field_name] = self.dat.demand[self.period_id_field_name] * 1.532
+        # Sample 1: modified data with non-integers on demand['Period ID']
+        dat1 = input_schema.copy_pan_dat(self.dat)
+        dat1.demand[self.period_id_field_name] = dat1.demand[self.period_id_field_name] * 1.532
         with self.assertRaises(ValueError):
-            check_each_period_id_column(self.dat)
+            check_each_period_id_column(dat1)
 
-        # Integer missing values on demand['Period ID']
-        self.dat = input_schema.csv.create_pan_dat(self.input_path)  # reset self.dat object
-        self.dat.demand.drop(0, axis=0, inplace=True)
+        # Sample 2: modified data with integer missing values on demand['Period ID']
+        dat2 = input_schema.copy_pan_dat(self.dat)
+        dat2.demand.drop(0, axis=0, inplace=True)  # remove first row
         with self.assertRaises(ValueError):
-            check_each_period_id_column(self.dat)
-
-        # We don't need a Bad PanDat object case, because when we call the check_each_period_id_column function,
-        # we have already created the PanDat object through input_schema.csv.create_pan_dat (or similar to xls file)
-        # so that TicDat would have already raised error for bad PanDat
+            check_each_period_id_column(dat2)
 
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main()
